895c1ec117390a1308a3b4c6a5433b221b0f1fed
findNextStep <findNextStep@gmail.com>
2018-07-14 19:42:18 +0800

    添加计时函数和一个简单的性能测试

--- include/test/map_test.hpp
+++ include/test/map_test.hpp
@@ -1,5 +1,6 @@
 #include "map_base/map_check.hpp"
 #include <string>
+#include <functional>
 
 
 namespace WMJTask {
@@ -11,5 +12,25 @@ namespace WMJTask {
  * @return false 测试不通过
  */
 bool mapInsertTest(::WMJTask::mapInterface < ::std::string, int> &yourmap);
+
+/**
+ * @brief 基于c++11的计时函数
+ * 使用样例
+ * cout << timeCount([&a,&b](){
+ *      testFunc(a,b);
+ *      testFunc2(a,b);
+ * }) << endl;
+ * @param func 待测试的函数类
+ * @return double 测试函数执行的时间，单位为秒
+ */
+double timeCount(const std::function<void()> &func);
+
+/**
+ * @brief 计算一个map的插入和查询的性能
+ * 
+ * @param yourmap 用于测试的map
+ * @return double 程序运行的时间，如果程序有误，会返回-1.0
+ */
+double mapPerformanceTest(::WMJTask::mapInterface<int, int> &yourmap);
 
 } // namespace WMJTask
--- src/test_lib/map_test.cpp
+++ src/test_lib/map_test.cpp
@@ -1,21 +1,63 @@
 #include "test/map_test.hpp"
 #include <iostream>
+#include <chrono>
 
 namespace WMJTask {
 
 using ::std::cout;
 using ::std::endl;
-bool mapInsertTest(::WMJTask::mapInterface < ::std::string, int> &yourmap) {
-    yourmap.insert(std::make_pair("10", 9));
-    if(yourmap.at("10") != 9) {
+
+double timeCount(const std::function<void()> &func) {
+    auto start = ::std::chrono::system_clock::now();
+    func();
+    auto end   = ::std::chrono::system_clock::now();
+    auto duration = ::std::chrono::duration_cast<::std::chrono::microseconds>(end - start);
+    return double(duration.count()) * ::std::chrono::microseconds::period::num / ::std::chrono::microseconds::period::den;
+}
+
+bool mapInsertTest(::WMJTask::mapInterface<::std::string, int> &yourmap) {
+    if(!yourmap.insert(std::make_pair("10", 9))) {
         cout << "插入pair失败" << endl;
         return false;
-    }
-
-    if(yourmap.insert("10", 9)) {
+    } else if(yourmap.at("10") != 9) {
+        cout << "插入pair失败" << endl;
+        return false;
+    } else if(yourmap.insert("10", 7) || yourmap.at("10") == 7) {
         cout << "插入重复数据" << endl;
         return false;
+    } else if(yourmap.size() != 1) {
+        cout << "size计算错误" << endl;
+        return false;
+    } else {
+        return true;
     }
-    return true;
+}
+
+double mapPerformanceTest(::WMJTask::mapInterface<int, int> &yourmap) {
+    bool result = true;
+    auto time = timeCount([&yourmap, &result]() {
+        for(int i = 0; i < 1000; ++i) {
+            yourmap.insert(std::make_pair(i, i));
+        }
+        for(int i = 0; i < yourmap.size(); ++i) {
+            if(yourmap.at(i) != i) {
+                cout << "map映射错误" << endl;
+                result = false;
+                return;
+            } else {
+                yourmap.at(i) = 10;
+                if(yourmap[i] != 10) {
+                    cout << "修改数据失败" << endl;
+                    result = false;
+                    return;
+                }
+            }
+        }
+    });
+    if(result) {
+        return time;
+    } else {
+        return -1.0;
+    }
 }
 } // namespace WMJTask
